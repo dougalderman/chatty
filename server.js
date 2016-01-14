6bcd380e6aa5ca1d4b136c7a8870284d99ce5468
Doug Alderman <agnadcp-d1@yahoo.com>
2016-01-13 17:29:48 -0700

    1/13/16 changes

--- server.js
+++ server.js
@@ -1,1 +1,51 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+
+app.listen(9254, function() {
+    console.log('Listening on port 9254');
+});
+
+var messages = ['From Chatty Server', 'I\'m an awesome server', 'I\'m located in Salt Lake City'];
+
+app.get('/', function(req, res) {
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send(JSON.stringify(messages));
+});
+
+app.post('/', function(req, res) {
+    console.log(req.body);
+    messages.push(req.body.message);
+    
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send(JSON.stringify(messages));
+    
+app.options('/', function(req, res) {
+    res.status(200).set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send();  
+});
+    
+  
+});
