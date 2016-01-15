f92940e4b16a79aa4815afd60d562c8448ef3e47
Doug Alderman <agnadcp-d1@yahoo.com>
2016-01-14 17:34:23 -0700

    1/14/16 changes

--- server.js
+++ server.js
@@ -22,9 +22,25 @@ app.get('/', function(req, res) {
     }).send(JSON.stringify(messages));
 });
 
+app.options('/', function(req, res) {
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+         'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io" 
+    }).send();  
+});
+
 app.post('/', function(req, res) {
     console.log(req.body);
-    messages.push(req.body.message);
+//    messages.push(req.body.message);
+    messages.push({
+        message: req.body.message,
+        time: new Date()
+    });
     
     res.status(200).set({
         'Content-Type': 'application/json',
@@ -36,16 +52,7 @@ app.post('/', function(req, res) {
         'Content-Security-Policy': "default-src 'self' devmountain.github.io"
     }).send(JSON.stringify(messages));
     
-app.options('/', function(req, res) {
-    res.status(200).set({
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-        'X-XSS-Protection': '1; mode=block',
-        'X-Frame-Options': 'SAMEORIGIN',
-        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
-    }).send();  
-});
+
     
   
 });
